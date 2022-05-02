d6b99103a2bbad175a953fe2651b3bb042755346
Michael Seifarth <mseifarth@babbel.com>
2022-05-02 14:55:57 +0200

    convert to functional component

--- react-typescript/src/DemoApp.tsx
+++ react-typescript/src/DemoApp.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import FullCalendar, {
   EventApi,
   EventClickArg,
@@ -21,171 +21,15 @@ import resourceDayGridPlugin from '@fullcalendar/resource-daygrid'
 import scrollGridPlugin from '@fullcalendar/scrollgrid'
 import momentPlugin from '@fullcalendar/moment'
 
-interface DemoAppState {
-  slotEventOverlap: boolean
-  datesAboveResources: boolean
-  groupByScale: boolean
-  currentEvents: EventApi[]
-}
+const Demo = () => {
+  const [slotEventOverlap, setSlotEventOverlap] = useState(false)
+  const [datesAboveResources, setDatesAboveResources] = useState(false)
+  const [groupByScale, setGroupByScale] = useState(false)
+  const [currentEvents, setCurrentEvents] = useState<EventApi[]>([])
 
-export default class DemoApp extends React.Component<{}, DemoAppState> {
-  state: DemoAppState = {
-    slotEventOverlap: false,
-    datesAboveResources: false,
-    groupByScale: false,
-    currentEvents: [],
-  }
-
-  render() {
-    return (
-      <div className='demo-app'>
-        {this.renderSidebar()}
-        <div className='demo-app-main'>
-          <FullCalendar
-            plugins={[
-              dayGridPlugin,
-              timeGridPlugin,
-              interactionPlugin,
-              listPlugin,
-              resourceTimelinePlugin,
-              resourceTimeGridPlugin,
-              resourceDayGridPlugin,
-              scrollGridPlugin,
-              momentPlugin,
-            ]}
-            headerToolbar={{
-              left: 'prev,next today',
-              center: 'title',
-              right:
-                'resourceDayGridDay,resourceDayGridWeek,resourceDayGridMonth resourceTimeGridDay,resourceTimeGridWeek resourceTimelineDay,resourceTimelineWeek,resourceTimelineMonth timeGridDay,timeGridWeek dayGridWeek,dayGridMonth listDay,listWeek',
-            }}
-            initialView='resourceTimelineWeek'
-            height='auto'
-            resourceAreaWidth='20%'
-            resourceGroupField={this.state.groupByScale ? 'scale' : undefined}
-            dayMinWidth={100}
-            editable={true}
-            selectable={false}
-            selectMirror={false}
-            dayMaxEvents={true}
-            allDaySlot={false}
-            slotEventOverlap={this.state.slotEventOverlap}
-            datesAboveResources={this.state.datesAboveResources}
-            slotDuration={'01:00:00'}
-            eventSources={[INITIAL_EVENTS, INITIAL_TEACHER_AVAILABILITIES]}
-            dateClick={this.handleDateClick}
-            eventContent={renderEventContent} // custom render function
-            eventClick={this.handleEventClick}
-            eventsSet={this.handleEvents} // called after events are initialized/added/changed/removed
-            /* you can update a remote database when these fire:
-            eventAdd={function(){}}
-            eventChange={function(){}}
-            eventRemove={function(){}}
-            */
-            resourceAreaHeaderContent={'Teachers'}
-            resources={INITIAL_TEACHERS}
-            resourceAreaColumns={[
-              {
-                field: 'title',
-                headerContent: 'Name',
-              },
-              {
-                field: 'availabilitiesCount',
-                headerContent: 'Total Availabilities',
-              },
-              {
-                field: 'scale',
-                headerContent: 'Modifier',
-              },
-            ]}
-          />
-        </div>
-      </div>
-    )
-  }
-
-  renderSidebar() {
-    return (
-      <div className='demo-app-sidebar'>
-        <div className='demo-app-sidebar-section'>
-          <h2>Instructions</h2>
-
-          <ul>
-            <li>Select dates and you will be prompted to create a new event</li>
-            <li>Drag, drop, and resize events</li>
-            <li>Click an event to delete it</li>
-          </ul>
-        </div>
-
-        <div className='demo-app-sidebar-section'>
-          <h2>Options</h2>
-
-          <ul>
-            <li>
-              <label>
-                <input
-                  type='checkbox'
-                  checked={this.state.slotEventOverlap}
-                  onChange={this.handleOverlapToggle}
-                ></input>
-                toggle overlap
-              </label>
-            </li>
-
-            <li>
-              <label>
-                <input
-                  type='checkbox'
-                  checked={this.state.datesAboveResources}
-                  onChange={this.handleDatesAboveResourcesToggle}
-                ></input>
-                dates above teachers
-              </label>
-            </li>
-
-            <li>
-              <label>
-                <input
-                  type='checkbox'
-                  checked={this.state.groupByScale}
-                  onChange={this.handleGroupByScaleToggle}
-                ></input>
-                group by scale
-              </label>
-            </li>
-          </ul>
-        </div>
-
-        <div className='demo-app-sidebar-section'>
-          <h2>All Events ({this.state.currentEvents.length})</h2>
-
-          <ul>{this.state.currentEvents.map(renderSidebarEvent)}</ul>
-        </div>
-      </div>
-    )
-  }
-
-  handleOverlapToggle = () => {
-    this.setState({
-      slotEventOverlap: !this.state.slotEventOverlap,
-    })
-  }
-
-  handleDatesAboveResourcesToggle = () => {
-    this.setState({
-      datesAboveResources: !this.state.datesAboveResources,
-    })
-  }
-
-  handleGroupByScaleToggle = () => {
-    this.setState({
-      groupByScale: !this.state.groupByScale,
-    })
-  }
-
-  handleDateClick = (selectInfo: DateClickArg) => {
-    let title = prompt('Please enter a new title for your event')
-    let calendarApi = selectInfo.view.calendar
+  const handleDateClick = (selectInfo: DateClickArg) => {
+    const title = prompt('Please enter a new title for your event')
+    const calendarApi = selectInfo.view.calendar
 
     calendarApi.unselect() // clear date selection
 
@@ -200,7 +44,7 @@ export default class DemoApp extends React.Component<{}, DemoAppState> {
     }
   }
 
-  handleEventClick = (clickInfo: EventClickArg) => {
+  const handleEventClick = (clickInfo: EventClickArg) => {
     if (
       confirm(
         `Are you sure you want to delete the event '${clickInfo.event.title}'`
@@ -210,24 +54,7 @@ export default class DemoApp extends React.Component<{}, DemoAppState> {
     }
   }
 
-  handleEvents = (events: EventApi[]) => {
-    this.setState({
-      currentEvents: events,
-    })
-  }
-}
-
-function renderEventContent(eventContent: EventContentArg) {
-  return (
-    <>
-      <b>{eventContent.timeText}</b>
-      <i>{eventContent.event.title}</i>
-    </>
-  )
-}
-
-function renderSidebarEvent(event: EventApi) {
-  return (
+  const SidebarEvent = (event: EventApi) => (
     <li key={event.id}>
       <b>
         {formatDate(event.start!, {
@@ -240,4 +67,131 @@ function renderSidebarEvent(event: EventApi) {
       {event.getResources().map((resource) => resource.title)}
     </li>
   )
+
+  const Sidebar = () => (
+    <div className='demo-app-sidebar'>
+      <div className='demo-app-sidebar-section'>
+        <h2>Instructions</h2>
+
+        <ul>
+          <li>Select dates and you will be prompted to create a new event</li>
+          <li>Drag, drop, and resize events</li>
+          <li>Click an event to delete it</li>
+        </ul>
+      </div>
+
+      <div className='demo-app-sidebar-section'>
+        <h2>Options</h2>
+
+        <ul>
+          <li>
+            <label>
+              <input
+                type='checkbox'
+                checked={slotEventOverlap}
+                onChange={() => setSlotEventOverlap((s) => !s)}
+              ></input>
+              toggle overlap
+            </label>
+          </li>
+
+          <li>
+            <label>
+              <input
+                type='checkbox'
+                checked={datesAboveResources}
+                onChange={() => setDatesAboveResources((s) => !s)}
+              ></input>
+              dates above teachers
+            </label>
+          </li>
+
+          <li>
+            <label>
+              <input
+                type='checkbox'
+                checked={groupByScale}
+                onChange={() => setGroupByScale((s) => !s)}
+              ></input>
+              group by scale
+            </label>
+          </li>
+        </ul>
+      </div>
+
+      <div className='demo-app-sidebar-section'>
+        <h2>All Events ({currentEvents.length})</h2>
+
+        <ul>{currentEvents.map(SidebarEvent)}</ul>
+      </div>
+    </div>
+  )
+
+  return (
+    <div className='demo-app'>
+      <Sidebar />
+
+      <div className='demo-app-main'>
+        <FullCalendar
+          plugins={[
+            dayGridPlugin,
+            timeGridPlugin,
+            interactionPlugin,
+            listPlugin,
+            resourceTimelinePlugin,
+            resourceTimeGridPlugin,
+            resourceDayGridPlugin,
+            scrollGridPlugin,
+            momentPlugin,
+          ]}
+          headerToolbar={{
+            left: 'prev,next today',
+            center: 'title',
+            right:
+              'resourceDayGridDay,resourceDayGridWeek,resourceDayGridMonth resourceTimeGridDay,resourceTimeGridWeek resourceTimelineDay,resourceTimelineWeek,resourceTimelineMonth timeGridDay,timeGridWeek dayGridWeek,dayGridMonth listDay,listWeek',
+          }}
+          initialView='resourceTimelineWeek'
+          height='auto'
+          resourceAreaWidth='20%'
+          resourceGroupField={groupByScale ? 'scale' : undefined}
+          dayMinWidth={100}
+          editable={true}
+          selectable={false}
+          selectMirror={false}
+          dayMaxEvents={true}
+          allDaySlot={false}
+          slotEventOverlap={slotEventOverlap}
+          datesAboveResources={datesAboveResources}
+          slotDuration={'01:00:00'}
+          eventSources={[INITIAL_EVENTS, INITIAL_TEACHER_AVAILABILITIES]}
+          dateClick={handleDateClick}
+          eventClick={handleEventClick}
+          eventsSet={setCurrentEvents} // called after events are initialized/added/changed/removed
+          /* you can update a remote database when these fire:
+          eventAdd={function(){}}
+          eventChange={function(){}}
+          eventRemove={function(){}}
+          */
+          resourceAreaHeaderContent={'Teachers'}
+          resources={INITIAL_TEACHERS}
+          resourceAreaColumns={[
+            {
+              field: 'title',
+              headerContent: 'Name',
+            },
+            {
+              field: 'availabilitiesCount',
+              headerContent: 'Total Availabilities',
+            },
+            {
+              field: 'scale',
+              headerContent: 'Modifier',
+            },
+          ]}
+        />
+      </div>
+    </div>
+  )
 }
+
+export default Demo
